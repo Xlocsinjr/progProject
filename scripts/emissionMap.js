ee095d15d39bc7a032e1ea26afb3cd87c97456d8
Xlocsinjr <Xlocsinjr@gmail.com>
2018-01-12 08:31:34 +0100

    slider tests

--- scripts/emissionMap.js
+++ scripts/emissionMap.js
@@ -3,28 +3,35 @@
  ****/
 
 var fakedata = [
-  [["USA", 10]],
-  [["USA", 8]],
-  [["USA", 4]],
-  [["USA", 5]],
-  [["USA", 7]]
+  [["USA", 10], ["CHN", 8]],
+  [["USA", 8], ["CHN", 7]],
+  [["USA", 4], ["CHN", 6]],
+  [["USA", 5], ["CHN", 7]],
+  [["USA", 7], ["CHN", 8]]
 ];
 
-console.log(fakedata[0]);
+
+
+// Copied from https://www.w3schools.com/howto/howto_js_rangeslider.asp
+var slider = document.getElementById("myRange");
+
+var yearIndex = slider.value;
+
+slider.oninput = function() {
+  yearIndex = slider.value;
+  console.log(data);
+};
 
 // Colour range
 var colour = d3.scale.linear()
   .range(["white", "blue"])
   .domain([0,10]);
 
+
 var map = new Datamap({
   element: document.getElementById('worldMap'),
   fills: {
     defaultFill: "#ABDDA4",
-    // 10: "#FF0E0F",
-    // 9: "FF2E2D",
-    // 8: "FF4847",
-    // 7: "FF6464",
   },
   data: {
     USA: { fillKey: 10 },
