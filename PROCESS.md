1591571ebd6d84cf1d8fec453c7d2731270752d3
Xlocsinjr <Xlocsinjr@gmail.com>
2018-01-21 15:35:19 +0100

    Fixed flickering tooltip in worldmap

--- PROCESS.md
+++ PROCESS.md
@@ -93,4 +93,6 @@ fully loaded after calling d3.json because it is asynchronous. It is best to
 just run all functions inside d3.json.
 - Investigated the colour flickering tooltip in the worldmap. It has definitely
 something to do with the slider in the html. Commenting the slider out in the
-html fixes the problem.
+html fixes the problem. Perhaps the slider causes the map to constantly reload.
+UPDATE: Apparently this issue can also be fixed by putting the slider after the
+world map in the html;.
--- code/scripts/testing.js
+++ code/scripts/testing.js
@@ -1,56 +1,49 @@
 /****
 
  ****/
- var map = new Datamap({
- element: document.getElementById('worldMap'),
- fills: {
-   defaultFill: "#ABDDA4",
- },
+ 
+// Copied from https://www.w3schools.com/howto/howto_js_rangeslider.asp
+var slider = document.getElementById("myRange");
+var yearIndex = slider.value;
+
+
+d3.json("../../data/jsons/mapData.json", function(error, data) {
+  if (error) throw error;
+
+  // ------------------- MAP ---------------------------------------------------
+  var map = new Datamap({
+    element: document.getElementById('worldMap'),
+    fills: {
+      defaultFill: "#ABDDA4",
+    },
+  });
+
+
+  // Colour range
+  var colour = d3.scale.linear()
+    .range(["#ABDDA4", "red"])
+    .domain([0, 12500000]);
+    // 12500000 : China 2012 GHG emission
+
+  function updateColour(yearIndex) {
+    updateDict = {};
+    keysList = Object.keys(data[yearIndex]);
+    for (i in keysList) {
+      key = keysList[i];
+      if (key != "year") {
+        val = data[yearIndex][key]["GHG"];
+        updateDict[key] = colour(val);
+      };
+    };
+    map.updateChoropleth(updateDict);
+  };
+
+  // ------------------- SLIDER UPDATE -----------------------------------------
+  updateColour(yearIndex);
+
+  slider.oninput = function() {
+    yearIndex = slider.value;
+    updateColour(yearIndex);
+    updateScatterYear(yearIndex);
+  };
 });
-
-
-// // Copied from https://www.w3schools.com/howto/howto_js_rangeslider.asp
-// var slider = document.getElementById("myRange");
-// var yearIndex = slider.value;
-//
-//
-// d3.json("../../data/jsons/mapData.json", function(error, data) {
-//   if (error) throw error;
-//
-//   // ------------------- MAP ---------------------------------------------------
-//   var map = new Datamap({
-//     element: document.getElementById('worldMap'),
-//     fills: {
-//       defaultFill: "#ABDDA4",
-//     },
-//   });
-//
-//
-//   // Colour range
-//   var colour = d3.scale.linear()
-//     .range(["#ABDDA4", "red"])
-//     .domain([0, 12500000]);
-//     // 12500000 : China 2012 GHG emission
-//
-//   function updateColour(yearIndex) {
-//     updateDict = {};
-//     keysList = Object.keys(data[yearIndex]);
-//     for (i in keysList) {
-//       key = keysList[i];
-//       if (key != "year") {
-//         val = data[yearIndex][key]["GHG"];
-//         updateDict[key] = colour(val);
-//       };
-//     };
-//     map.updateChoropleth(updateDict);
-//   };
-//
-//   // ------------------- SLIDER UPDATE -----------------------------------------
-//   updateColour(yearIndex);
-//
-//   slider.oninput = function() {
-//     yearIndex = slider.value;
-//     updateColour(yearIndex);
-//     updateScatterYear(yearIndex);
-//   };
-// });
